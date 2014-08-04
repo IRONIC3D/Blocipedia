5306db0eb56a7872ceede6d73e0b75ee8e684749
Iyad Horani <i3d@ironic3d.com>
2014-08-05 07:17:32 +1000

    Add user seed data, add association between wikis and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :wikis
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
+  def full_name
+    first_name + " " + last_name
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,23 @@
 require 'faker'
 
+5.times do
+  user = User.create(
+    first_name:     Faker::Name.first_name,
+    last_name:      Faker::Name.last_name,
+    email:          Faker::Internet.email,
+    password:       Faker::Lorem.characters(10)
+  )
+end
+users = User.all
+
 20.times do
   Wiki.create(
+    user:     users.sample,
     title:    Faker::Lorem.sentence,
     body:     Faker::Lorem.paragraph
   )
 end
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Wiki.count} wikis created"

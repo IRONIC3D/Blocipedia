0af97a01fb2607c2d564025d843510a3abbd0964
Iyad Horani <i3d@ironic3d.com>
2014-08-05 06:28:24 +1000

    modified wikis#new wikis#edit and wikis#destroy, add flash messages

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,8 +8,38 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+    if @wiki.save
+      flash[:notice] = "Wiki was saved"
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again"
+      render :new
+    end
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+      flash[:notice] = "Wiki was updated succefully"
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again"
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    @wiki.destroy
+    redirect_to wikis_path
   end
 end

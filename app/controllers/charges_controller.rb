7ed52a8e1586af75f49be33c2a749d02a8300e08
Iyad Horani <i3d@ironic3d.com>
2014-08-07 07:52:44 +1000

    Add drop down menu for user when signed in

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,41 @@
+class ChargesController < ApplicationController
+  def new
+    @stripe_btn_data = {
+     key: "#{ Rails.configuration.stripe[:publishable_key] }",
+     description: "Blocipedia Monthly Subscription - #{current_user.name}",
+     amount: 500 
+     # We're like the Snapchat for Wikipedia. But really, 
+     # change this amount. Stripe won't charge $9 billion.
+   }
+  end
+
+  def create
+    # Amount in centes
+    @amount = params[:amount]
+
+    # Create a Stripe customer object, for associating with the charge
+    customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken]
+    )
+
+    # Where the real magic happens
+    charge = Stripe::Charge.create(
+      customer: customer.id, # Note -- this is NOT the user_id
+      amount: @amount,
+      description: "Blocipedia Monthly Subscription",
+      currency: 'aud'
+    )
+
+    flash[:success] = "Thank you"
+    redirect_to user_path(current_user)
+
+    # Stripe will send back CardErrors, with friendly messages
+    # when something goes wrong.
+    # This `rescue block` catches and displays those errors.
+
+  rescue Stripe::CardErrors => e
+    flash[:error] = e.messages
+    redirect_to new_charge_path
+  end
+end

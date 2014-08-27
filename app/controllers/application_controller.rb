1206394ad34735faf1fe72f1dab3d853d20fe7d9
Iyad Horani <i3d@ironic3d.com>
2014-08-27 14:23:36 +1000

    Change redirect message to be descriptive

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from Pundit::NotAuthorizedError do |exception|
-    redirect_to root_url, alert: exception.message
-    # TODO: Put a generic message that is more descriptive.
+    redirect_to root_url, alert: "You need to be logged in to view the Wikis."
   end
 
   def after_sign_in_path_for(resource)
